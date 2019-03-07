cc21a5110d02ae46af202f2fd8a1bafa3d15dbf9
kapphire <ken.sapphire87@yahoo.com>
2019-03-07 12:28:15 +0800

    whitenoise added

--- interShoptrace/settings/base.py
+++ interShoptrace/settings/base.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,6 +147,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # FIREBASE DATABASE CONFIGURATION
 FIREBASE_CONFIG = {
