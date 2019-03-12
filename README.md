c842c98c481fdf25f06ae44ab6f7902d92647cff
kapphire <ken.sapphire87@yahoo.com>
2019-03-12 17:58:13 +0800

    redbeat redis url updated

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ For Windows :
 - celery beat -S redbeat.RedBeatScheduler -A interShoptrace.celeryapp:app --pidfile= --loglevel=debug
  
 - celery worker -Q inventory -A interShoptrace.celeryapp:app -l info -P gevent
+
+- SECRET_KEY : fl!3*dy@g%99b*#t5i+@zoc$yogoqk#col8i2fyeb&818144l&
+- DJANGO_SETTINGS_MODULE : interShoptrace.settings.staging
--- interShoptrace/celeryapp.py
+++ interShoptrace/celeryapp.py
@@ -6,7 +6,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'interShoptrace.settings.develop
 app = Celery('interShoptrace')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-app.conf.redbeat_redis_url = 'redis://localhost:6379/0'
+app.conf.redbeat_redis_url = os.getenv('REDIS_URL', 'redis//localhost:6379/0')
 app.conf.broker_pool_limit = 1
 app.conf.broker_heartbeat = None
 app.conf.broker_connection_timeout = 30
